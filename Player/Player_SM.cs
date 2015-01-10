c67779e638b2ca7ee6fa3933c42511fdc89257e5
kushalponam <kushal.ponam@gmail.com>
2015-01-10 16:42:30 +0530

    Player Classes

---
+++ Player/Player_SM.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+/*Attach this class to player object in any scene where 2d slection is required
+ * 
+*/
+public class Player_SM : MonoBehaviour {
+   // the slected object is stored in this vaiable
+    private SelectObjects objselection;
+    private GameManager GM;
+    private CalibrationManager CM;
+    private Vector2 DirectionVector;
+    private AnalogStick analogstick;
+    // Sensitivity of the cursor movement on screen
+    public float Sensitivity;
+
+	void Start () {
+        objselection = GetComponent<SelectObjects>();
+        GM = GameManager.Instance;
+        CM = CalibrationManager.Instance;
+	    analogstick = AnalogStick.Instance;
+       // Debug.Log(Screen.height + " " + Screen.width);
+#if UNITY_WEBPLAYER
+        AdjustCameraOnWebBuild();
+#endif
+#if UNITY_ANDROID
+        AdjustCameraOnWebBuild();
+#endif
+	}
+	void Update () {
+        // Get the direction vector from the joystick
+	    if (GM.IsAndroid && !GM.IsCalibrationRequired)
+	    {
+	        DirectionVector.x = analogstick.GetAxis().x;
+	        DirectionVector.y = analogstick.GetAxis().y * -1;
+	    }
+	    else if (GM.IsCalibrationRequired)
+        {
+             DirectionVector = new Vector2(CM.GetDirection(0), CM.GetDirection(1));
+        }
+        else if(!GM.IsAndroid)
+        {
+             DirectionVector = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        }
+       // if the cursor is within the screen coordinates
+        if (IsWithinScreen())
+        {
+            objselection.left += (DirectionVector.x) * Sensitivity;
+            objselection.top += (DirectionVector.y) * Sensitivity;
+            //Debug.Log(DirectionVector+"  "+objselection.left + " " +objselection.top);
+        }
+        CheckBorders();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.LoadLevel("NewCity");
+        }
+	}
+
+    // we are setting the camera's orthographic size 
+    // because of web build . 
+    void AdjustCameraOnWebBuild()
+    {
+        Camera.main.orthographicSize = 66;
+    }
+
+    bool IsWithinScreen()
+    {
+        if ((objselection.left >= 0 && objselection.top >= 0) && (objselection.left <= Screen.width && objselection.top <= Screen.height)) return true;
+        return false;
+    }
+    void CheckBorders()
+    {
+        if (objselection.left < 0) objselection.left = 0;
+        if (objselection.top < 0) objselection.top = 0;
+        if (objselection.left > Screen.width) objselection.left = Screen.width;
+        if (objselection.top > Screen.height) objselection.top = Screen.height;
+    }
+   
+    
+}
---
+++ Player/SelectObjects.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+/* Attach this class to Player
+ * this class does raycasting to objects that will be picked or selected
+*/
+
+public class SelectObjects : MonoBehaviour {
+	public Texture2D crosshairImage;
+	Ray ray;
+
+    [HideInInspector]
+	public float left,top;
+	
+    RaycastHit hit;
+	public GameObject SelectedObject;
+	public bool Selected = false;
+    public bool SelectedArrow = false;
+	private Color SelectedColor = new Color(0.23f,0.95f,0.21f,0.35f);
+	private Color DeSelectedColor = new Color(0,0,0,0);
+    private DrawObjectiveList drawobjective;
+
+    private bool CantakeScreenShot=false;
+
+    private float SelectionImageWidth;
+    private float SelectionImageHeight;
+
+    private static SelectObjects s_Instance = null;
+    public static SelectObjects Instance
+    {
+        get
+        {
+            if (s_Instance == null)
+            {
+                s_Instance = FindObjectOfType(typeof(SelectObjects)) as SelectObjects;
+                if (s_Instance == null)
+                {
+                    Debug.Log("Could not locate Selectobject");
+                }
+            }
+            return s_Instance;
+        }
+    }
+    void Start()
+    {
+        left = Screen.width / 2;
+        top = Screen.height / 2;
+        SelectionImageWidth = crosshairImage.width / 4;
+        SelectionImageHeight = crosshairImage.height / 4;
+        drawobjective = DrawObjectiveList.Instance;
+        CantakeScreenShot = true;    
+#if UNITY_WEBPLAYER
+        SelectionImageWidth = crosshairImage.width / 5;
+        SelectionImageHeight = crosshairImage.height / 5;
+#endif
+#if UNITY_ANDROID
+        SelectionImageWidth = crosshairImage.width / 5;
+        SelectionImageHeight = crosshairImage.height / 5;
+#endif
+    }
+
+	void OnGUI()
+	{
+        // this is just to test on scenes without loading game manager.
+        if (drawobjective == null)
+        {
+            GUI.DrawTexture(new Rect(left, top, SelectionImageWidth, SelectionImageHeight), crosshairImage);
+            return;
+        }
+        if (drawobjective.MinimizeObjective)
+        {
+            GUI.DrawTexture(new Rect(left, top, SelectionImageWidth, SelectionImageHeight), crosshairImage);
+        }
+	}
+
+    #region WriteData
+    public string WriteGitterObjectName;
+    private float elapsedTime = 0;
+    void WriteData()
+    {
+        if (DataCollector.Instance == null || ObjectiveManager.Instance.GetCurrentObjective == null) return;
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= 0.2f)
+        {
+            DataCollector.Instance.WriteGitter(ObjectiveManager.Instance.GetScore, ObjectiveManager.Instance.Level, GameTimer.Instance.timetext, ObjectiveManager.Instance.GetCurrentObjective.Description, WriteGitterObjectName,left,top);
+            WriteGitterObjectName = "None";
+            elapsedTime = 0;
+        }
+    }
+    #endregion
+
+    #region CaptureScreenShot
+    private float elapsedTimeforScreenShot = 0;
+    void CaptureScreenShot()
+    {
+        if (!CantakeScreenShot ||DrawObjectiveList.Instance == null || DataCollector.Instance==null || GameTimer.Instance==null) return;
+        if (CantakeScreenShot && Application.loadedLevelName != "NewCity" && DrawObjectiveList.Instance.MinimizeObjective)
+        {
+            //delay to let the objective window minimize completely
+            elapsedTimeforScreenShot += Time.fixedDeltaTime;
+            if (elapsedTimeforScreenShot > 1.5f)
+            {
+                DataCollector.Instance.CaptureScreenShot(Application.loadedLevelName, GameTimer.Instance.timetext);
+                CantakeScreenShot = false;
+                elapsedTimeforScreenShot = 0;
+            }
+        }
+        else
+        {
+            elapsedTimeforScreenShot = 0;
+        }
+    }
+    #endregion
+    void Update()
+	{
+     // Debug.Log(Input.mousePosition);
+	  ray = Camera.main.ScreenPointToRay(new Vector3(left + SelectionImageWidth/2,Screen.height-(top+20),0));
+	  Debug.DrawRay(ray.origin,ray.direction,Color.red);
+	  if(Physics.Raycast(ray,out hit,100))
+		{
+            if (hit.collider.tag == "item_1")
+            {
+                //   Debug.Log("Hitting ");
+                Selected = true;
+                SelectedObject = hit.collider.gameObject;
+                SelectedObject.transform.FindChild("Cylinder").renderer.material.color = SelectedColor;
+            }
+            else if (hit.collider.tag == "directionalarrow")
+            {
+                SelectedArrow = true;
+                SelectedObject = hit.collider.gameObject;
+                SelectedObject.transform.FindChild("Cylinder").renderer.material.color = SelectedColor;
+            }else if(hit.collider.tag == "SelectionArrows")
+            {
+                SelectedObject = hit.collider.gameObject;
+                SelectedObject.transform.FindChild("Cylinder").renderer.material.color = SelectedColor;
+                SelectedObject.GetComponent<BankSelection>().Select();
+            }
+            else
+            {
+                Selected = false;
+                SelectedArrow = false;
+                if (SelectedObject != null)
+                {
+                    SelectedObject.transform.FindChild("Cylinder").renderer.material.color = DeSelectedColor;
+                }
+            }
+		}
+      WriteData();
+	}
+
+    void FixedUpdate()
+    {
+        CaptureScreenShot();
+    }
+
+}
